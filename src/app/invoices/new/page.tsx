16f2f7d1a1559aa911236daa901ab43dc6ea8877
Rodrigo <rod_novaes@hotmail.com>
2024-12-21 17:45:32 +0100

    Update invoices and dashboard page to use the Container component

--- src/app/invoices/new/page.tsx
+++ src/app/invoices/new/page.tsx
@@ -7,6 +7,7 @@ import { Label } from "@/components/ui/label";
 import { Textarea } from "@/components/ui/textarea";
 import { createAction } from "@/app/actions";
 import SubmitButton from "@/components/SubmitButton";
+import Container from "@/components/Container";
 
 export default function Invoices() {
   const [state, setState] = useState("ready");
@@ -21,47 +22,49 @@ export default function Invoices() {
   }
 
   return (
-    <main className='flex flex-col justify-center gap-6 h-full max-w-5xl mx-auto my-12'>
-      <div className='flex justify-between'>
-        <h1 className='text-3xl font-semibold'>Create Invoice</h1>
-      </div>
+    <main className='h-full'>
+      <Container>
+        <div className='flex justify-between mb-6'>
+          <h1 className='text-3xl font-semibold'>Create Invoice</h1>
+        </div>
 
-      <Form
-        action={createAction}
-        onSubmit={handleOnSubmit}
-        className='grid gap-4 max-w-xs'
-      >
-        <div>
-          <Label htmlFor='name' className='block font-semibold text-sm mb-2'>
-            Billing Name
-          </Label>
-          <Input id='name' name='name' type='text' />
-        </div>
-        <div>
-          <Label htmlFor='email' className='block font-semibold text-sm mb-2'>
-            Billing Email
-          </Label>
-          <Input id='email' name='email' type='email' />
-        </div>
-        <div>
-          <Label htmlFor='value' className='block font-semibold text-sm mb-2'>
-            Value
-          </Label>
-          <Input id='value' name='value' type='text' />
-        </div>
-        <div>
-          <Label
-            htmlFor='description'
-            className='block font-semibold text-sm mb-2'
-          >
-            Description
-          </Label>
-          <Textarea id='description' name='description' />
-        </div>
-        <div>
-          <SubmitButton />
-        </div>
-      </Form>
+        <Form
+          action={createAction}
+          onSubmit={handleOnSubmit}
+          className='grid gap-4 max-w-xs'
+        >
+          <div>
+            <Label htmlFor='name' className='block font-semibold text-sm mb-2'>
+              Billing Name
+            </Label>
+            <Input id='name' name='name' type='text' />
+          </div>
+          <div>
+            <Label htmlFor='email' className='block font-semibold text-sm mb-2'>
+              Billing Email
+            </Label>
+            <Input id='email' name='email' type='email' />
+          </div>
+          <div>
+            <Label htmlFor='value' className='block font-semibold text-sm mb-2'>
+              Value
+            </Label>
+            <Input id='value' name='value' type='text' />
+          </div>
+          <div>
+            <Label
+              htmlFor='description'
+              className='block font-semibold text-sm mb-2'
+            >
+              Description
+            </Label>
+            <Textarea id='description' name='description' />
+          </div>
+          <div>
+            <SubmitButton />
+          </div>
+        </Form>
+      </Container>
     </main>
   );
 }
