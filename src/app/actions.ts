e46088e899b75610691f092d7383f83448109e90
Rodrigo <rod_novaes@hotmail.com>
2025-01-14 21:05:55 +0100

    Update the actions with userId

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -1,18 +1,25 @@
 "use server";
 
 import { redirect } from "next/navigation";
+import { auth } from "@clerk/nextjs/server";
 import { Invoices } from "@/db/schema";
 import { db } from "@/db";
 
 export async function createAction(formData: FormData) {
+  const { userId } = await auth();
   const value = Math.floor(parseFloat(String(formData.get("value"))) * 100);
   const description = formData.get("description") as string;
+
+  if (!userId) {
+    return;
+  }
 
   const results = await db
     .insert(Invoices)
     .values({
       value,
       description,
+      userId,
       status: "open",
     })
     .returning({ id: Invoices.id });
