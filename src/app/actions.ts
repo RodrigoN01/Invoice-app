9ba95eb5880b2381c605663a2a8a1ce533f650b8
Rodrigo <rod_novaes@hotmail.com>
2025-01-18 12:20:32 +0100

    Add customer query to the actions

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -3,18 +3,30 @@
 import { redirect } from "next/navigation";
 import { auth } from "@clerk/nextjs/server";
 import { revalidatePath } from "next/cache";
-import { Invoices, Status } from "@/db/schema";
+import { Customers, Invoices, Status } from "@/db/schema";
 import { db } from "@/db";
 import { and, eq } from "drizzle-orm";
 
 export async function createAction(formData: FormData) {
   const { userId } = await auth();
-  const value = Math.floor(parseFloat(String(formData.get("value"))) * 100);
-  const description = formData.get("description") as string;
 
   if (!userId) {
     return;
   }
+
+  const value = Math.floor(parseFloat(String(formData.get("value"))) * 100);
+  const description = formData.get("description") as string;
+  const name = formData.get("name") as string;
+  const email = formData.get("email") as string;
+
+  const [customer] = await db
+    .insert(Customers)
+    .values({
+      name,
+      email,
+      userId,
+    })
+    .returning({ id: Customers.id });
 
   const results = await db
     .insert(Invoices)
@@ -22,6 +34,7 @@ export async function createAction(formData: FormData) {
       value,
       description,
       userId,
+      customerId: customer.id,
       status: "open",
     })
     .returning({ id: Invoices.id });
